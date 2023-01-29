8827e513a1c1ae2ff9e7ab18a24368c02bc65319
dfu7 <78887201+dfu7@users.noreply.github.com>
2023-01-29 12:20:11 -0800

    scene switching

---
+++ Assets/Scripts/SceneStuffs.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneStuffs : MonoBehaviour
+{
+    #region singleton
+    private static SceneStuffs _instance;
+
+    public static SceneStuffs Instance { get { return _instance; } }
+    
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(Instance);
+    }
+    #endregion
+
+    void Update()
+    {
+        HandleSceneSwitching();
+    }
+
+    void HandleSceneSwitching()
+    {
+        // next scene
+        if (Input.GetKeyDown(KeyCode.RightArrow) && SceneManager.sceneCountInBuildSettings != SceneManager.GetActiveScene().buildIndex + 1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        // last scene
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
